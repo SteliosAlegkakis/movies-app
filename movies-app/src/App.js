ef5cda1c6097b8164ba5d0488edfcc77846cd16e
SteliosAlegkakis <stelios.alegak@gmail.com>
2023-06-10 23:58:59 +0300

    added react router

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Search from "./search";
 import Results from "./results";
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
@@ -9,10 +10,16 @@ function App() {
   const [category,setCategory] = useState("movie");
 
   return (
-    <div className="App">
-      <Search setSearch={setSearch} setCategory={setCategory}></Search>
-      <Results search={search} category={category}></Results>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Search setSearch={setSearch} setCategory={setCategory}></Search>
+            <Results search={search} category={category}></Results>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
