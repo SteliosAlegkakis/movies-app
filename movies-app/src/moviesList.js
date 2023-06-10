07933a38c1a8bd9174075a2f46e77d530e4f4258
SteliosAlegkakis <stelios.alegak@gmail.com>
2023-06-10 22:02:12 +0300

    added sort button

--- movies-app/src/moviesList.js
+++ movies-app/src/moviesList.js
@@ -3,7 +3,7 @@ const MoviesList = (data) => {
     const movies = data.movies;
     if(movies.Response==='False'){
         return(
-            <div>
+            <div className="results-number">
                 <h3>Search Results (0)</h3>
             </div>
         )
@@ -11,7 +11,13 @@ const MoviesList = (data) => {
     
     return ( 
         <div>
-            <h3>Search Results ({movies.Search.length})</h3>
+            <div className="results-sort">
+                <h3 className="results-number">Search Results ({movies.Search.length})</h3>
+                <select>
+                    <option>Year</option>
+                    <option>A-Z</option>
+                </select>
+            </div>
             {movies.Search.map(movie => (
                 <div className="movies-list" key={movie.imdbID}>
                     <div className="movie-preview"  key={movie.imdbID}>
